56d9f0a99453b5ba442f37c7a5e840ceac86ac14
S.L. Wachyu <slwachyu@gmail.com>
2025-10-25 12:49:39 +0700

    fix(oauth): clean up Google OAuth2 handler and aggregator flow

--- src/services/aggregator/aggregator.service.js
+++ src/services/aggregator/aggregator.service.js
@@ -1,9 +1,6 @@
-import { PrismaClient } from '@prisma/client'
+import { prisma } from '../../lib/prisma.js'
 import logger from '../../config/logger.js'
 import { redis, isRedisEnabled } from '../../config/redis.js'
-
-// === Prisma Client ===
-const prisma = new PrismaClient()
 
 // === Environment Variables ===
 const BPS_API_KEY = process.env.BPS_API_KEY || ''
