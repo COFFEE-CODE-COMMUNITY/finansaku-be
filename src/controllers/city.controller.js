0b9a114556c7d6aff09b56f3f2b8171a27501f4e
hannahm-04 <hannahanifam@gmail.com>
2025-12-15 14:41:23 +0700

    feat: add get cities API

--- src/controllers/city.controller.js
+++ src/controllers/city.controller.js
@@ -1,12 +1,25 @@
-import { getAllCities } from "../services/city.service.js"
+import { getAllCities, getCitiesPaginated } from "../services/city.service.js"
 
 export const getCities = async (req, res, next) => {
   try {
-    const cities = await getAllCities()
+    const { all, page = 1, limit = 20 } = req.query
+
+    let cities
+
+    // FE mau ambil SEMUA (dropdown, dll)
+    if (all === "true") {
+      cities = await getAllCities()
+    } else {
+      // default pagination
+      cities = await getCitiesPaginated({
+        page: Number(page),
+        limit: Number(limit),
+      })
+    }
 
     res.status(200).json({
       success: true,
-      message: "Get all cities success",
+      message: "Get cities success",
       data: cities,
     })
   } catch (error) {
